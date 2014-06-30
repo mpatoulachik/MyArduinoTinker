2230fc5dc1d873ab0e8501f4c01bfda2f84c0950
M.Patoulachik <mpatoulachik@yahoo.com>
2014-06-30 19:22:07 +0200

    RPM reading is working !

--- PID Sync/PotandRpmReading/PotandRpmReading.ino
+++ PID Sync/PotandRpmReading/PotandRpmReading.ino
@@ -30,7 +30,9 @@ Motor as M3
 Optosensor as I2
 Pot as A0
 
-
+Timekeeping logic is following: The "Counting" flag keeps tracks of wher in the process we are. First time we wait for a rising edge, counter=0 . When it comeswe set the start time and counter=1 (we are measuring time).
+Next time a rising edge comes we save the measured time, put "counting" to 0 and detach the interrupt so the rest of the program has a chance to run a complete loop.
+So basically you just stand on the side of the racetrack, wait until the car passes to start your stopwatch and stop it the next time the car passes again.
 
 */
 
@@ -56,7 +58,7 @@ int ledPin = 13;    //onboard led
 int potPin = 0;     //Potentiometer wiper
 AF_DCMotor motor(3); // create motor #3, 
 volatile bool ledState = LOW;             //State of the led
-volatile unsigned long counter = 0;       //Tick counter
+volatile unsigned long counter = 0;       //Tick counter, mostly used for debug
 unsigned long newMillis = 0;              //time counting
 volatile unsigned long microStart = 0;              //time counting for RPM counter
 float rpm = 0.0;
@@ -100,7 +102,7 @@ void loop()
 
   motor.run(FORWARD);                     // turn motor on going forward
 
-rpm = (float)elapsedMicros * (float)nTeeth / 1000000.0 * 60.0; //// This calc is fucked up, but the rest seems to work.
+rpm =  ((360.0 / (float)nTeeth) / ((float)elapsedMicros / 1000000.0) / 360.0) * 60.0 ; //Calculating the RPM value from the detected ticks
 
 
 
@@ -123,7 +125,7 @@ rpm = (float)elapsedMicros * (float)nTeeth / 1000000.0 * 60.0; //// This calc is
 
                 newMillis = millis();
               }
-attachInterrupt(0, state, RISING);      //activate the interrupt
+attachInterrupt(0, state, RISING);      //activate the interrupt when we are finished with the loop
 }
   
   
@@ -142,12 +144,40 @@ attachInterrupt(0, state, RISING);      //activate the interrupt
 void state(){                             //This is the function called by the interrupt
 	ledState = !ledState;              //toggle the led state
   	++ counter;                             //increment the counter
-    if(counting == 1){
-        elapsedMicros = micros() - microStart;
-        counting = 0;
-        detachInterrupt(0);
+    if(counting == 1){                      // If the stopwatch is already running
+        elapsedMicros = micros() - microStart;    //Then save the lap time 
+        counting = 0;                              // And stop the stopwatch
+        detachInterrupt(0);                   //This will allow the loop to run completely at leat once. otherwise there is a risk that it gets interrupted before it is done
     } else {
-        microStart = micros();
-        counting = 1;
+        microStart = micros();              //Reset the stopwatch
+        counting = 1;                       //Start the stopwatch
     }
 }
+
+
+
+
+
+
+//  ████████╗ ██████╗ ██████╗  ██████╗ 
+//  ╚══██╔══╝██╔═══██╗██╔══██╗██╔═══██╗
+//     ██║   ██║   ██║██║  ██║██║   ██║
+//     ██║   ██║   ██║██║  ██║██║   ██║
+//     ██║   ╚██████╔╝██████╔╝╚██████╔╝
+//     ╚═╝    ╚═════╝ ╚═════╝  ╚═════╝ 
+//                                     
+
+/*
+-Handle micros() overflow
+-Handle the case where the motor is stopped :
+    maybe calculate elapsedmicros at the end of the loop as well
+    or maybe add some kind of timeout
+-Understand why only 2 decimals are shown in the serial monitor
+-Interrupt both on rising and faling edge. That way you double the resolution of the encoder. (provided the notches on the wheel are as long as the tops)
+
+
+
+
+
+
+*/
