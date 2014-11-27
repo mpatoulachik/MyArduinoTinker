4e07111741af5608718f87b66d31819b38888584
M.Patoulachik <mpatoulachik@yahoo.com>
2014-11-27 20:24:05 +0100

    It's Alive !

--- Ultimaker Suicide with remote plugs/Controlling_the_plug/Controlling_the_plug.ino
+++ Ultimaker Suicide with remote plugs/Controlling_the_plug/Controlling_the_plug.ino
@@ -8,17 +8,24 @@
 
 RCSwitch mySwitch = RCSwitch();
 int ledpin = 13;
+int onPin = 8;
+int offPin = 9;
 
 
 void setup() {
+
 pinMode(ledpin, OUTPUT);
+pinMode(8, INPUT_PULLUP);
+pinMode(9, INPUT_PULLUP);
   // Transmitter is connected to Arduino Pin #10  
   mySwitch.enableTransmit(10);
+
 
   
   // Optional set pulse length.
   // mySwitch.setPulseLength(320);
-  
+  Serial.begin(9600);
+  Serial.println("Start");
 }
 
 void loop() {
@@ -28,16 +35,24 @@ void loop() {
   // A to D (A=1, B=2 and so on..) or I to IV depending on the brand.
   // 
   // The second parameter represents the secondsetting of the rotary switch (1 to 4 in my case). 
-  mySwitch.switchOn(3, 2);
-  digitalWrite(ledpin,HIGH);
-
+  if(digitalRead(onPin) == LOW){
+   
+    mySwitch.switchOn(3, 2);
+    digitalWrite(ledpin,HIGH);
+    Serial.println ("ON");
+    }
   // Wait a second
-  delay(1000);
+  //delay(1000);
   
   // Switch off
-  mySwitch.switchOff(3, 2);
-  digitalWrite(ledpin,LOW);
-  // Wait another second
-  delay(1000);
+  if(digitalRead(offPin) == LOW){
+       
+    mySwitch.switchOff(3, 2);
+    digitalWrite(ledpin,LOW);
+    Serial.println ("Off");
+   }
+
+   // Wait another second
+  //delay(1000);
   
 }
